7929904a9d0c68697afacbf0a2f0584af7ba8869
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2021-06-28 20:13:08 +0100

    Add files via upload

--- master/workers.py
+++ master/workers.py
@@ -19,15 +19,22 @@ class RenderWorker:
         return hash(self.tag)
 
     async def handle_reader(self):
-        while True:
-            msg = await self.ws.receive_json()
-            render_id = msg['id']
-            render = msg['render']
-            fut = self.handles.pop(render_id)
-            fut.set_result(render)
+        try:
+            while True:
+                msg = await self.ws.receive_json()
+                render_id = msg['id']
+                render = msg['render']
+                fut = self.handles.pop(render_id)
+                fut.set_result(render)
+        except:
+            return
 
     async def handle_writes(self):
-        while True:
-            render_id, fut = await self.events.get()
-            self.handles[str(render_id)] = fut
-            await self.ws.send_json({"id": str(render_id)})
+        try:
+            while True:
+                render_id, fut = await self.events.get()
+                self.handles[str(render_id)] = fut
+                await self.ws.send_json({"id": str(render_id)})
+        except:
+            return
+
